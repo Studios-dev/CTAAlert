a60aecf327202407aed4086c6c7740d25b92a6e1
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2025-02-01 22:54:03 -0600

    Ignore on local

--- index.ts
+++ index.ts
@@ -74,10 +74,10 @@ const postUpdatesCronAction = async () => {
 		let mastodonID: string | undefined = existingAlert?.mastodonId;
 
 		// Twitter is broken on deploy currently
-		const allowTwitter = false
+		const allowTwitter = Deno.env.get("DENO_DEPLOYMENT_ID") == undefined;
 
 		if (
-			allowTwitter && 
+			allowTwitter &&
 			isTwitterBlocked.value == undefined &&
 			(twitterID == undefined ||
 				existingAlert?.lastMessage != alertMessage)
